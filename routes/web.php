c475dbb57008aaf83be64c430c1355aa8651b214
Vicky AS <vickyhuang808@gmail.com>
2023-07-09 09:21:39 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn()=>view('welcome'));
 
 Route::get('/hello', function () {
     return view('hello', [
